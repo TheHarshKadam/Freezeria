c0b2d540fb70e8971687bf42b3c4be71392a758a
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-05-21 00:15:28 +0530

    Update user.dart

--- lib/req_info/user.dart
+++ lib/req_info/user.dart
@@ -1,5 +1,25 @@
+
 class User {
   final String uid;
 
   User({this.uid});
+}
+
+class UserData{
+  final String uid;
+  final String Name;
+  final String CupSize;
+  final int Blend;
+  final String Flavour;
+  final String Cream;
+  final String Toppings;
+
+  UserData(
+      {this.uid,
+      this.Blend,
+      this.Cream,
+      this.CupSize,
+      this.Flavour,
+      this.Name,
+      this.Toppings});
 }
